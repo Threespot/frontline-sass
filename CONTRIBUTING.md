20329068ce715a8b66232136c32d03af5f08fb85
Ted Whitehead <ted.whitehead@threespot.com>
2015-10-29 02:56:59 -0400

    Add files

---
+++ CONTRIBUTING.md
@@ -0,0 +1,31 @@
+The Frontline Sass team welcomes contributions. While contributing, you must adhere to the project [code of conduct](#the-code-of-conduct), so that everyone feels welcome.
+
+Here’s how you can make Frontline Sass better:
+
+  - [Report an issue](https://github.com/threespot/frontline-sass/issues/new) or ask questions
+  - [Suggest new features](https://github.com/threespot/frontline-sass/issues/new)
+  - Check, vet, diagnose, or add details to existing issues.
+  - Contributing or improving documentation. We’re sure there are still typos or unclear instructions. These are easy wins.
+  - Contributing pull requests or bugfixes.
+
+**Please note:** Frontline Sass is not necessarily the team’s full-time gig. Please be patient with us getting back to you.
+
+Thanks for helping us make Frontline Sass better!
+
+### The Code of Conduct
+
+Frontline Sass strongly values contributors from anywhere, regardless of gender, gender expression, sexual orientation, ability, physical appearance, body type, race, age, or religion. As a result, the team has agreed to and enforces this code of conduct in order to provide a harassment-free experience for everyone who participates in the development of Frontline Sass.
+
+Harassment includes offensive comments related to gender, gender expression, sexual orientation, ability, physical appearance, body type, race, age, or religion. It also includes the posting of sexual images, deliberate intimidation, stalking, and unwelcome sexual attention.
+
+Further, harassment includes ad-hominem attacks, grandstanding, derogatory comments, trolling, public or private community disruption, insults, or other unprofessional conduct.
+
+Harassment is not tolerated in our issue tracker, code, events, chat channels, over emails, or any other way you can communicate with a contributor. Anyone who is asked to cease harassment is expected to comply immediately. Our team is not exempt from this policy.
+
+If anyone engages in harassing behavior, we may take appropriate action, up to and including warning the offender, deletion of comments, removal from the project’s codebase and communication systems, and escalation to GitHub support.
+
+If you are being harassed, notice that someone else is being harassed, or have any other concerns, please open an issue or contact a team member.
+
+### License
+
+This document is published from the United States. To the extent possible under law, Threespot Media has waived all copyright and related or neighboring rights to this document worldwide. The text may be re-used for any purpose and without credit.
---
+++ Gulpfile.js
@@ -0,0 +1,121 @@
+'use strict';
+
+// -----------------------------------------------------------------------------
+// Dependencies
+// -----------------------------------------------------------------------------
+
+var fs = require('fs');
+var gulp = require('gulp');
+var cssimport = require('gulp-cssimport');
+var plugins = require('gulp-load-plugins')();
+var packageInfo = require('./package.json');
+var sassdoc = require('sassdoc');
+var path = require('path');
+var gh = require('gh-pages');
+var yaml = require('js-yaml');
+var sassLint = require('gulp-sass-lint');
+var scsslint = require('gulp-scss-lint');
+
+// -----------------------------------------------------------------------------
+// Dist
+// -----------------------------------------------------------------------------
+
+gulp.task('build', function () {
+  return gulp
+    .src('src/_frontline.scss')
+    .pipe(cssimport())
+    .pipe(plugins.header(fs.readFileSync('./banner.txt', 'utf8')))
+    .pipe(plugins.header('@charset \'UTF-8\';\n\n'))
+    .pipe(plugins.replace(/@version@/, packageInfo.version))
+    .pipe(gulp.dest('./dist/_frontline.scss'));
+});
+
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+gulp.task('test:libsass', function () {
+  return gulp
+    .src(['./tests/*.scss'])
+    .pipe(plugins.sass());
+});
+
+gulp.task('test:rubysass', function () {
+  return plugins
+    .rubySass('./tests', { stopOnError: true })
+    .on('error', function (err) {
+      process.exit(1);
+    });
+});
+
+
+gulp.task('test', ['build'], function () {
+  gulp.start('test:libsass');
+  gulp.start('test:rubysass');
+});
+
+
+// -----------------------------------------------------------------------------
+// Lint
+// -----------------------------------------------------------------------------
+
+gulp.task('scss-lint', function() {
+  return gulp.src('./src/*/*.scss')
+    .pipe(scsslint());
+});
+
+gulp.task('sass-lint', function() {
+  return gulp.src('./src/*/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
+
+// -----------------------------------------------------------------------------
+// Sass API documentation
+// -----------------------------------------------------------------------------
+
+gulp.task('sassdoc', function () {
+  var options = yaml.safeLoad(fs.readFileSync('.sassdocrc', 'utf-8'));
+  options.dest = './sassdoc/documentation';
+  options.verbose = true;
+  options.package = require('./package.json');
+
+  return gulp
+    .src('src/**/*.scss')
+    .pipe(sassdoc(options))
+    .resume();
+});
+
+
+// -----------------------------------------------------------------------------
+// GH-pages task
+// -----------------------------------------------------------------------------
+
+gulp.task('gh-pages', ['build', 'sassdoc'], function () {
+  gh.publish(path.join(__dirname, 'sassdoc'), {
+    add: true,
+    message: 'Automatic SassDoc update from Gulp',
+    logger: function(message) {
+      console.log(message);
+    }
+  }, function(err) {
+    if (err) console.log(err);
+  });
+});
+
+
+// -----------------------------------------------------------------------------
+// Default task
+// -----------------------------------------------------------------------------
+
+gulp.task('default', ['build', 'test']);
+
+
+// -----------------------------------------------------------------------------
+// Deploy documentation task
+// -----------------------------------------------------------------------------
+
+gulp.task('deploy', ['gh-pages']);
