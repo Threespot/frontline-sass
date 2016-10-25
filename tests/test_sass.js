35ae13113e17673aa6dbf0898ad3fb854c66da81
Mat Brady <matbr8dy@gmail.com>
2016-10-25 16:51:55 -0400

    adding variable test

--- tests/test_sass.js
+++ tests/test_sass.js
@@ -1,8 +1,22 @@
 var path     = require('path');
 var sassTrue = require('sass-true');
 
-// var sassFile = path.join(__dirname, 'test.scss');
-// sassTrue.runSass({file: sassFile}, describe, it);
+// Files to test
+// ENHANCEMENT: glob files
+const testFiles = [
+  'variables/_fs-base-font-size.scss',
+  'variables/_fs-breakpoints.scss',
+  'variables/_fs-colors.scss',
+  'variables/_fs-easings.scss',
+  'variables/_fs-svg-icons.scss',
+  'variables/_fs-zindex.scss',
+];
+
+// Loop through each file name and run the test file.
+testFiles.forEach(function(file) {
+  let sassFile = path.join(__dirname, file);
+  sassTrue.runSass({file: sassFile}, describe, it);
+});
 
 var sassFile = path.join(__dirname, 'functions/_color.scss');
 sassTrue.runSass({file: sassFile}, describe, it);
