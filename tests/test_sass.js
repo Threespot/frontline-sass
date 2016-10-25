0f6f7fad61d0157bb8ef7569ae8d4591305afd1f
Mat Brady <matbr8dy@gmail.com>
2016-10-25 18:30:40 -0400

    adding em function test and updating docs

--- tests/test_sass.js
+++ tests/test_sass.js
@@ -4,12 +4,17 @@ var sassTrue = require('sass-true');
 // Files to test
 // ENHANCEMENT: glob files
 var testFiles = [
+  // Variables
   'variables/_fs-base-font-size.scss',
   'variables/_fs-breakpoints.scss',
   'variables/_fs-colors.scss',
   'variables/_fs-easings.scss',
   'variables/_fs-svg-icons.scss',
   'variables/_fs-zindex.scss',
+  // Functions
+  'functions/_color.scss',
+  'functions/_em.scss'
+  // Mixins
 ];
 
 // Loop through each file name and run the test file.
@@ -17,6 +22,3 @@ testFiles.forEach(function(file) {
   var sassFile = path.join(__dirname, file);
   sassTrue.runSass({file: sassFile}, describe, it);
 });
-
-var sassFile = path.join(__dirname, 'functions/_color.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
